c39b6f7814d66740ca5cde98608412db5e0b1934
Brian Crowe <brian.j.crowe@protonmail.com>
2025-07-16 21:28:02 -0400

    MVP reached for Project 2

--- js/script.js
+++ js/script.js
@@ -3,28 +3,90 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
+//showPage function - takes in: list as array of objects, page as an argument to determine selected page #
+const showPage = (list, page = 1) => {
+  //startIndex takes in the number of the page, multiplies it by the number of cards on page (9)
+  // - then subtracts 9 so on page 1, startIndex will be 0
+  const startIndex = page * 9 - 9
+  //endIndex takes multiplies the page number by 9 to find the index of the last student to be displayed on page
+  const endIndex = page * 9
 
+  //student list gets the unordered list with the class "student-list"
+  const studentList = document.querySelector('ul.student-list')
 
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
+  //set studentList's innerHTML to empty string to remove any students that were previously displayed
+  studentList.innerHTML = ''
 
+  //for loop to go over list of students
+  for (let i = 0; i < list.length; i++) {
+    //conditional to make sure we're only going over the students that are supposed to be on the page
+    if (i >= startIndex && i < endIndex) {
+      //html taken from instructions page - in template literal
+      const student = list[i]
+      const html = `
+      <li class='student-item cf'>
+        <div class='student-details'>
+          <img class='avatar' src='${student.picture.large}' alt='Profile Picture'>
+          <h3>${student.name.first} ${student.name.last}</h3>
+          <span class='email'>${student.email}</span>
+        </div>
+        <div class='joined-details'>
+          <span class='date'>Joined ${student.registered.date}</span>
+        </div>
+      </li>
+      `
+      //insert elements using insertAdjacentHTML and beforeend
+      studentList.insertAdjacentHTML('beforeend', html)
+    }
+  }
+}
 
+//addPagination function responsible for rendering pagination buttons at the bottom of the page
+const addPagination = (list) => {
+  //determine number of buttons with Math.ceil because even if there's only 1 student on the next page, they need to be displayed.
+  const numberOfButtons = Math.ceil(list.length / 9)
+  //find and store link-list ul
+  const linkList = document.querySelector('ul.link-list')
 
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
+  //use innerHTML property to set link-list to empty string to remove any previously generated pagination buttons
+  linkList.innerHTML = ''
 
+  //loop over number of buttons - start with 1 because people don't deal with 0 index
+  for (let i = 1; i <= numberOfButtons; i++) {
+    const html = `
+    <li>
+      <button type='button'>${i}</button>
+    </li>
+    `
 
+    //add buttons to html
+    linkList.insertAdjacentHTML('beforeend', html)
+  }
+  //select first pagination button and set class to active
+  linkList.querySelector('button').classList.add('active')
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
+  //create event listener to listen for clicks on the .link-list element
+  linkList.addEventListener('click', (e) => {
+    //find the active button
+    const activeButton = linkList.querySelector('.active')
+    //look for clicked button
+    const buttonClicked = e.target.closest('button')
 
+    //if there is an active button and a button is clicked:
+    if (activeButton && buttonClicked) {
+      //remove active from the active button
+      activeButton.classList.remove('active')
+    }
 
+    //add active to new button, if clicked
+    if (buttonClicked) {
+      buttonClicked.classList.add('active')
+      let newPage = Number(buttonClicked.innerHTML)
+      showPage(data, newPage)
+    }
+  })
+}
 
 // Call functions
+showPage(data, 1)
+addPagination(data)
